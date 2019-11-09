afefe88570bde43cd138e32b2f528f524d4f1114
Mario McGee <mario.mcgee@students.makeschool.com>
2019-11-08 18:38:00 -0800

    reversed funciton complete

--- backward-poetry.py
+++ backward-poetry.py
@@ -1,1 +1,21 @@
-Print("hello")
+
+
+txt = "Do not stand at my grave and weep,I am not there; I do not sleep,I am a thousand winds that blow, I am the diamond glints on snow, I am the sun ripened grain, I am the gentle autum rain, When you awaken in the morning's hush, I am the swift uplifting rush, Of quiet birds in circled flight, I am the soft stars that shine at night, Do not stand at my grave and cry, I am not there; I did not die! "
+#Source: https://www.familyfriendpoems.com/poem/do-not-stand-by-my-grave-and-weep-by-mary-elizabeth-frye
+
+
+
+
+def lines_printed_backward(txt):
+    x = txt.split(", ")
+    for word in reversed(x):
+        print(word)
+
+
+
+
+
+#def lines_printed_randomly(my_poem):
+    #this function prints the powem in radnomly
+
+print (lines_printed_backward(txt))
