1aa6609535392e22e65bf30ce68435051d1a6838
Mario McGee <mario.mcgee@students.makeschool.com>
2019-11-08 20:16:45 -0800

    made a vortex

--- backward-poetry.py
+++ backward-poetry.py
@@ -1,3 +1,5 @@
+# from random import shuffle
+# import random
 
 
 txt = "Do not stand at my grave and weep,I am not there; I do not sleep,I am a thousand winds that blow, I am the diamond glints on snow, I am the sun ripened grain, I am the gentle autum rain, When you awaken in the morning's hush, I am the swift uplifting rush, Of quiet birds in circled flight, I am the soft stars that shine at night, Do not stand at my grave and cry, I am not there; I did not die! "
@@ -6,16 +8,46 @@ txt = "Do not stand at my grave and weep,I am not there; I do not sleep,I am a t
 
 
 
-def lines_printed_backward(txt):
-    x = txt.split(", ")
-    for word in reversed(x):
-        print(word)
+# def lines_printed_backward(txt):
+#     x = txt.split(", ")
+#     for word in reversed(x):
+#         print(word)
 
 
 
 
+# def lines_printed_rand(txt):
+#     random_lines=[]
+#     x = txt.split(', ') 
+#     for lines in x:
+#         random_lines.append(lines)
 
-#def lines_printed_randomly(my_poem):
-    #this function prints the powem in radnomly
+#     random.shuffle(random_lines)
+#     y = len(random_lines)
 
-print (lines_printed_backward(txt))
+#     for i in range(0, y): 
+#         print (random_lines[i])
+    #  x = txt.split(', ')
+    #  for word in x:
+    #      print(random.shuffle(word))
+def idk (txt):
+    string = txt   
+    length = len(txt)
+    for row in range(length):
+        for col in range(row+1):
+            print(string[col], end="")
+
+
+            
+
+
+idk(txt)
+
+
+    # x = txt.split('n, ')
+    # for i in range(n, 0, -3):
+    #     print(x * i)
+#print (lines_printed_backward(txt))
+# lines_printed_rand(txt)
+# triangle(txt)
+
